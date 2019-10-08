6032f509b51338cd6e76325de832b1bdb21fbecd
john <john@spinningplates.org>
2019-10-08 14:18:59 -0700

    mvp

---
+++ apple-nav/src/data.js
@@ -0,0 +1,100 @@
+export const data = [
+  {
+    id: 100,
+    type: 'Mac',
+    sub: 'mac'
+  },
+  {
+    id: 101,
+    type: 'iPad',
+    sub: 'ipad'
+  },
+  {
+    id: 102,
+    type: 'iPhone',
+    sub: 'iphone'
+  },
+  {
+    id: 103,
+    type: 'Watch',
+    sub: 'watch'
+  },
+  {
+    id: 104,
+    type: 'TV',
+    sub: 'tv'
+  },
+  {
+    id: 105,
+    type: 'Music',
+    sub: 'music'
+  }
+];
+
+export const mac = [
+  'MacBook Air',
+  'MacBook Pro',
+  'iMac',
+  'iMac Pro',
+  'Mac Pro',
+  'Mac mini',
+  'Compare',
+  'Pro Display XDR',
+  'Accessories',
+  'Catalina'
+];
+
+export const ipad = [
+  'iPad Pro',
+  'iPad Air',
+  'iPad',
+  'iPad mini',
+  'Compare',
+  'Apple Pencil',
+  'Smart Keyboard',
+  'Accessories',
+  'iPadOS'
+]
+
+export const iphone = [
+  'iPhone 11 Pro',
+  'iPhone 11',
+  'iPhone XR',
+  'iPhone 8',
+  'Compare',
+  'Apple Card',
+  'AirPods',
+  'Accessories',
+  'iOS 13'
+]
+
+export const watch = [
+  'Apple Watch Series 5',
+  'Apple Watch Nike',
+  'Apple Watch Hermes',
+  'Apple Watch Edition',
+  'Apple Watch Series 3',
+  'Compare',
+  'Bands',
+  'AirPods',
+  'Accessories',
+  'watchOS'
+]
+
+export const tv = [
+  'Apple TV app',
+  'Apple TV+',
+  'Apple TV 4K',
+  'Apple TV HD',
+  'AirPlay',
+  'Accessories'
+]
+
+export const music = [
+  'Apple Music',
+  'HomePod',
+  'AirPods',
+  'iPod touch',
+  'Music Accessories',
+  'Gift Cards'
+]
